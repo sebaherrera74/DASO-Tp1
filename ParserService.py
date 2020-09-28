8341523be7a6892f5e1734190a188d251295819e
Sebastian Herrera <sebastianh@hotmail.com.ar>
2020-09-28 19:42:27 -0300

    integro las pruebas que hice en prueba.py para parsear y enviar por el socket al ParserService.py

--- ParserService.py
+++ ParserService.py
@@ -1,45 +1,79 @@
-import socket
+import csv
 import json
 import os
+import socket
 import sys
+import time
 
 #Cliente UDP 
+
+#Clase para acceder a archivo .csv y armar la lista para enviarla por el socket 
+class Csv:
+    def __init__(self,file_csv):
+        self.set_csv_name(file_csv)
+        
+    def set_csv_name(self,file_csv):
+        self.__csv_name=file_csv
+        
+    def get_csv_name(self):
+        return self.__csv_name
+
+    def leer_csv(self):
+        lista=[]
+        with open(self.get_csv_name(),newline='') as File:
+            reader = csv.reader(File)
+            for row in reader:
+                elemento={"id":row[0],"nombre":row[1],"value1":(row[2]),"value2":row[3]}
+                #print(elemento)
+                lista.append(elemento)
+                #print(lista)
+                lista.pop(0) #elimino el primer elemento de la lista 
+                print(lista)
+                #jsonData = json.dumps(lista)
+                #print(jsonData)
 
 
 
 class Main:
+    def __init__(self,file_name):
+        self.set_file_name(file_name)
+    
+    def set_file_name(self,file_name):
+        self.__file_name=file_name
 
-    def __init__(self):
-        pass
+    def get_file_name(self):
+        return self.__file_name
+
+    def get_file_csv(self):
+        with open (self.get_file_name(),"r",encoding="utf-8") as f:
+            texto=f.read()
+        return texto.strip() #Saco espacios en blanco del archivo, solo mando el .csv
 
     def main(self):
-
         port = 10000
+        server_address=("localhost",port)
+        print('conectado a {}, puerto {}'.format(server_address[0],server_address[1]))
         try:
             port = int(sys.argv[1])
         except:
             print("Puerto incorrecto")
             exit(1)
+        #Hardcodeo para probar 
+        #data=[{'id': '1', 'name': 'Dolar', 'value1': '58.63', 'value2': '61.61'}, {'id': '2', 'name': 'Euro', 'value1': '65.12', 'value2': '68.93'}, {'id': '3', 'name': 'Real', 'value1': '13.45', 'value2': '14.23'}]
 
-        data=[{"id": 1, "value1": 80, "value2": 30, "name": "Dolar"}, {"id": 2,"value1": 65, "value2": 70, "name": "Euro"}]
-        coding=json.dumps(data)
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        s.sendto(bytearray(coding,"utf-8"),("localhost",port))
-        
-        (data, addr) = s.recvfrom(128*1024)    #Veo mensaje recibido desde server
-        print("Mensaje Recibido :"+data)       #Imprimo mensaje recibido 
-       
         #while True:
-            #(data, addr) = s.recvfrom(128*1024)
-            #data = Parser.parseData(data)
-            #self.model.updateData(data)
-            #self.view.show()
-            #s.sendto(bytearray("OK","utf-8"),addr)
-        
-        #finally:
-            #
-         #   print("Cierro Socketpass") 
-          #  s.close()
 
-m = Main()
+            #coding=json.dumps(data)
+            #print(coding)
+            #s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            #s.sendto(bytearray(coding,"utf-8"),server_address)
+            
+            #(data, addr) = s.recvfrom(128*1024)    #Veo mensaje recibido desde server
+            #print("Mensaje Recibido :"+str(data))       #Imprimo mensaje recibido                         
+
+
+
+
+print("hola")
+m=Main('config.txt')
 m.main()
--- prueba.py
+++ prueba.py
@@ -1,3 +1,36 @@
+import csv
+import time
+import json
+
+class Csv:
+    def __init__(self,file_csv):
+        self.set_csv_name(file_csv)
+    
+    def set_csv_name(self,file_csv):
+        self.__csv_name=file_csv
+    
+    def get_csv_name(self):
+        return self.__csv_name
+
+    def leer_csv(self):
+        lista=[]
+        with open(self.get_csv_name(),newline='') as File:
+            reader = csv.reader(File)
+            for row in reader:
+                elemento={"id":row[0],"nombre":row[1],"value1":(row[2]),"value2":row[3]}
+                #print(elemento)
+                lista.append(elemento)
+                
+            #print(lista)
+            lista.pop(0) #elimino el primer elemento de la lista 
+            print(lista)
+            
+
+            #jsonData = json.dumps(lista)
+            #print(jsonData)
+           
+
+        
 
 class Main:
     def __init__(self,file_name):
@@ -11,8 +44,8 @@ class Main:
     
     def main(self):
         with open (self.get_file_name(),"r",encoding="utf-8") as f:
-            texto=f.readline()
-        return texto
+            texto=f.read()
+        return texto.strip() #Saco espacios en blanco del archivo, solo mando el .csv
 
 
       
@@ -20,3 +53,7 @@ class Main:
 m = Main("config.txt")
 prueba=m.main()
 print(prueba)
+
+c=Csv(prueba)
+print(type(c))
+Csv.leer_csv(c)
